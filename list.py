308550d7463d297d0ad2aab21884f62f4c3969a6
didoxgusenga <97796777+didoxgusenga@users.noreply.github.com>
2025-05-17 12:48:08 +0300

    Add files via upload
    This is a file contains List and Append method to existing list

---
+++ list.py
@@ -0,0 +1,10 @@
+
+names = ["Kagetera", "John", "Peter", "Alex", "Claude"]
+
+names.append("Dieudonne")
+names.append("University")
+names.append("Gusenga")
+
+names.sort(reverse=True)
+
+print(names)
